364365258497806f35888069965939db7aedca70
Wojciech Kaczmarski <w.kaczmarski@teletra.pl>
2024-01-10 10:34:06 +0100

    pragma once for term.h

--- Core/Inc/term.h
+++ Core/Inc/term.h
@@ -5,13 +5,10 @@
  *      Author: SP5WWP
  */
 
-#ifndef INC_TERM_H_
-#define INC_TERM_H_
+#pragma once
 
 #define TERM_CLR		"\033c"			//clear terminal
 #define TERM_GREEN		"\033[92m"
 #define TERM_RED		"\033[91m"
 #define TERM_YELLOW		"\033[33m"
 #define TERM_DEFAULT	"\033[39m"
-
-#endif /* INC_TERM_H_ */
